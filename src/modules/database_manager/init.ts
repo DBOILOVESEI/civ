d292f7580c7be6ea836b3384a3b3acee2d1b9a96
EI <darlinshot@gmail.com>
2026-04-13 13:43:02 +0700

    Add Table initialization to DatabaseManager.

--- src/modules/database_manager/init.ts
+++ src/modules/database_manager/init.ts
@@ -1,15 +1,38 @@
 import Database from "better-sqlite3";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { pathToFileURL } from "url"; 
 import config from "./config.json" with { type:"json" };
 
 // MAIN
 const DatabaseManager = {};
 
 DatabaseManager.Init = async (client) => {
-  DatabaseManager.Database = new Database(config.DATABASE_NAME);
+  const dbPath = join(import.meta.dirname, config.DATABASE_PATH, config.DATABASE_NAME);
+  DatabaseManager.Database = new Database(dbPath);
   DatabaseManager.Database.pragma("foreign_keys = ON");
   DatabaseManager.Database.pragma("journal_mode = WAL")
   
   console.log("Database initialization complete.")
+  
+  const tablesPath = join(import.meta.dirname, config.TABLES_PATH);
+  const tables = readdirSync(tablesPath);
+  for (const tableModule of tables) {
+    const moduleURL = pathToFileURL(join(tablesPath, tableModule, config.INIT_FILE)).href;
+    const table = (await import(moduleURL)).default;
+
+    if (!("Init" in table)) {
+      console.log(`TableModule ${tableModule} in ${tablesPath} is missing Init function.`)
+      return;
+    };
+
+    table.Init(client, DatabaseManager.Database);
+    for (const funcName in table) {
+      if (config.INDEX_FUNCTION_IGNORE.includes(funcName)) {continue;};
+      console.log(funcName)
+    };
+  };
+
 };
 
 export default DatabaseManager;
---
+++ src/modules/database_manager/tables/poi/init.ts
@@ -0,0 +1,12 @@
+const Table = {};
+
+Table.Init = async (client, database) => {
+  console.log("Hello!!!!!!")
+
+};
+
+Table.IAmCoolHuiikStinks = async (client, database) => {
+  
+};
+
+export default Table;
